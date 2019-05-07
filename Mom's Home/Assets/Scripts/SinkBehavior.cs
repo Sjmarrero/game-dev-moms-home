2bfee250c27726929fcc614fbb4185f27379d712
Steve <stevemarrero11@gmail.com>
2019-05-07 15:56:19 -0700

    washing dishes mechanic added + setting table

--- Mom's Home/Assets/Scripts/SinkBehavior.cs
+++ Mom's Home/Assets/Scripts/SinkBehavior.cs
@@ -2,28 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlateBehavior : MonoBehaviour
+public class SinkBehavior : MonoBehaviour
 {
-    bool clean;
     // Start is called before the first frame update
     void Start()
     {
-        clean = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void setClean()
-    {
-        clean = true;
-    }
-
-    public bool isClean()
-    {
-        return clean;
     }
 }
